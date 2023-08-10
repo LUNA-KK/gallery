ebd4550868d496fe0da6c947b0e380829990e74b
LUNA-KK <110970349+LUNA-KK@users.noreply.github.com>
2023-08-10 20:46:47 +0700

    gallery4

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Art from "./Art";
+import Title from "./Title";
+import Loading from "./Loading";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Like from "./Like";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Title />
+        <Routes>
+          <Route path="/" element={<Art />} />
+          <Route path="/like" element={<Like />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
